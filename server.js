674507de3432d586d7a6528871581c82cd34c5a2
amirSohel007 <amirsohel27@gmail.com>
2020-06-01 21:35:18 +0530

    fix deployment

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/url-shortner'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/url-shortner/index.html'));});
+app.listen(process.env.PORT || 8080);
